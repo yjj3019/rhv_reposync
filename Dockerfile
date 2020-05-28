b611987a94c6131c728c057c3a225c24b7a4df95
root <magicsoma@hanmail.net>
2020-05-27 22:10:07 -0400

    rhv_reposync initial

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM registry.access.redhat.com/rhel7
+MAINTAINER jjyoo@rockplace.co.kr
+LABEL "RHEL7 reposync with Podman"
+LABEL summary="RHEL7 base reposync image"
+
+### Timezone
+ENV TZ=Asia/Seoul
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+### Source IP
+ENV sip=""
+
+#ENV Path
+ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+RUN mkdir -p /repo1 > /dev/null
+
+### Package Update
+RUN yum update -y > /dev/null
+
+### Package Install
+RUN yum install cronie createrepo -y > /dev/null
+RUN yum repolist --disablerepo=* && \
+    yum-config-manager --disable \* > /dev/null && \
+    yum-config-manager --enable rhel-7-server-rhv-4.3-manager-rpms --enable rhel-7-server-rhv-4.2-manager-rpms --enable rhel-7-server-rhv-4.1-manager-rpms --enable rhel-7-server-rhv-4-manager-rpms --enable rhel-7-server-rhv-4-manager-tools-rpms > /dev/null 
+RUN yum clean all -y > /dev/null
+
+### Cron Setting
+# Seems like a container specific issue on Centos: https://github.com/CentOS/CentOS-Dockerfiles/issues/31 
+RUN sed -i '/session    required   pam_loginuid.so/d' /etc/pam.d/crond
+
+### Cron Add
+ADD start-cron.txt /tmp/start-cron.txt
+RUN crontab /tmp/start-cron.txt 
+RUN rm -f /tmp/start-cron.txt
+
+### reposync file Add
+RUN mkdir -p /root/reposync
+ADD rhv_reposync.sh /root/reposync
+ADD rhv_channel.txt /root/reposync
+RUN chmod +x /root/reposync/rhv_reposync.sh
+
+##custom entry point — needed by cron
+COPY entrypoint /entrypoint
+RUN chmod +x /entrypoint
+ENTRYPOINT ["/entrypoint"]
---
+++ rhv_reposync.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -x
+
+export repo_dir="/repo1"
+export todate=`date +%Y%m%d`
+export totime=`date +%Y%m%d-%H%M%S`
+export repofile="/repo1/logs/reposync-rhv.log.$totime"
+export tmppath="/root/bin/temp"
+export fpath="/repo1/rhv.repo"
+
+### yum file check
+if [ -f /var/run/yum.pid ]; then
+echo "Start Fail.. YUM Run File Found : $repofile " >> $repofile
+echo "Time : $totime " >> $repofile
+exit 11
+fi
+
+### repofile check
+if [ -f $repofile ]
+then
+	echo "Found File : $repofile " >> $repofile
+else
+	touch $repofile
+fi
+
+### Log Directory Check
+if [ ! -d $repo_dir/logs ]; then
+	echo "$repo_dir/logs Not Found " >> $repofile
+	mkdir -p $repo_dir/logs
+fi
+
+### Old Logs File Delete
+/usr/bin/find $repo_dir/logs -mtime +30 -exec rm -f {} \;
+
+### Start Time 
+echo "Start Time : $totime " >> $repofile
+
+### repo file Create
+echo "#### Local Repository ####" > $fpath
+echo "#Create by : $totime" >> $fpath
+echo "" >> $fpath
+echo "" >> $fpath
+
+
+
+echo "-------------------------------------Start-------------------------------------------" >> $repofile
+for repos in $(cat /root/reposync/rhv_channel.txt)
+do
+echo "-------------------------------------$repos-------------------------------------------" >> $repofile
+### reposync
+
+if [ -d $repo_dir/$repos ]
+then
+	/usr/bin/reposync --gpgcheck -l --newest-only --downloadcomps --download-metadata -r $repos --download_path=$repo_dir >> $repofile 2>&1
+	echo "" >> $repofile
+	createrepo $repo_dir/$repos >> $repofile 2>&1
+else
+	/usr/bin/reposync --gpgcheck -l --downloadcomps --download-metadata -r $repos --download_path=$repo_dir >> $repofile 2>&1
+	echo "" >> $repofile
+	createrepo $repo_dir/$repos >> $repofile 2>&1
+fi
+
+### repo file Create
+  echo "[$repos]" >> $fpath
+  echo "name=$repos" >> $fpath
+  echo "baseurl=http://$sip/$repos" >> $fpath
+  echo "enabled=1" >> $fpath
+  echo "gpgcheck=0" >> $fpath
+  echo "" >> $fpath
+  echo "" >> $fpath
+
+done
+
+echo "----------------------------------------END------------------------------------------" >> $repofile
+
+exit;
